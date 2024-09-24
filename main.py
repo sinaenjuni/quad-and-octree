37bc199059f0f87ba3d87b304143144c1b89026b
sinaenjuni <y2k4388@gmail.com>
2024-09-24 18:58:46 +0900

    DEV: query functions
    I developed two functions for searching through rectangular and circle-based.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 from matplotlib.backend_bases import MouseButton
+from matplotlib.patches import Rectangle, Circle
+
 import numpy as np
+np.random.seed(123)
 
 from quadtree import QuadTree, Point
 
@@ -28,9 +31,20 @@ if __name__ == "__main__":
     w, h = 200, 200
     n = 100
     points = np.random.rand(n, 2) * w
+    qt = QuadTree(0, 0, w, h, 2, 0)
+    for x, y in points:
+        ret = qt.insert(Point(x, y))
 
-    DPI = 60
-    fig = plt.figure(figsize=(1440/DPI, 720/DPI), dpi=DPI)
+    found_boxsize = (40, 40)
+    found_radius = 40
+    # found_points = []
+    # qt.query(100,100, 50, 50, found_points)
+    # for fount_point in found_points:
+        # print(fount_point)
+
+
+    DPI = 50
+    fig = plt.figure(figsize=(720/DPI, 720/DPI), dpi=DPI)
     ax = plt.subplot()
 
     plt.connect('close_event', on_close)
@@ -42,10 +56,10 @@ if __name__ == "__main__":
 
     plt.ion() # turn interactive mode on
     while 1:
-        qt = QuadTree(0, 0, w, h, 2, 0)
-        points += (np.random.randn(n, 2) * 0.1)
-        for x, y in points:
-            ret = qt.insert(Point(x, y))
+        # qt = QuadTree(0, 0, w, h, 2, 0)
+        # points += (np.random.randn(n, 2) * 0.1)
+        # for x, y in points:
+            # ret = qt.insert(Point(x, y))
 
         plt.cla()
         ax.set_xlim(0, w)
@@ -55,8 +69,34 @@ if __name__ == "__main__":
         qt.draw(ax)
 
         ax.scatter(mouse_position[0], mouse_position[1], 200)
+        query_pos_xmin = mouse_position[0]-found_boxsize[0]/2
+        query_pos_ymin = mouse_position[1]-found_boxsize[1]/2
+        query_pos_xmax = found_boxsize[0]
+        query_pos_ymax = found_boxsize[1]
+
+        ax.add_patch(Rectangle((query_pos_xmin, query_pos_ymin,), 
+                                query_pos_xmax, query_pos_ymax,
+                            edgecolor='blue',
+                            facecolor='none',
+                            lw=1))
+        
+        ax.add_patch(Circle((mouse_position[0], mouse_position[1]), 
+                            found_radius, fill=False))
+        
+        found_points = []
+        qt.query(mouse_position[0], mouse_position[1],
+                    found_boxsize[0], found_boxsize[1], found_points)
+        
+        qt.query_circle(mouse_position[0], mouse_position[1],
+                        found_radius, found_points)
+        for fount_point in found_points:
+            ax.scatter(fount_point.x, fount_point.y, 500, "r", "*")
+            # print(fount_point, end=" ")
+        # print()
+
+        
         plt.draw()
-        plt.pause(0.01)
+        plt.pause(1e-4)
 
         if is_close:
             break
--- quadtree.py
+++ quadtree.py
@@ -1,9 +1,16 @@
+import numpy as np
 from matplotlib.patches import Rectangle
 
 class Point:
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
+    
+    def distance_to(self, other):
+        return np.hypot(*(self - other))
+    
+    def __sub__(self, other):
+        return np.array([self.x - other.x, self.y - other.y])
     
     def __str__(self) -> str:
         return "({}, {})".format(self.x, self.y)
@@ -75,7 +82,51 @@ class QuadTree:
             elif self.BR.insert(point):
                 return True                    
         return -1
+    
+    def query(self, cx, cy, w, h, found_points):
+        xmin = cx - w/2 
+        xmax = cx + w/2 
+        ymin = cy - h/2 
+        ymax = cy + h/2 
 
+        if (xmax < self.xmin or xmin > self.xmax or
+            ymax < self.ymin or ymin > self.ymax):
+            return False
+
+        for point in self.points:
+            if (xmin <= point.x and point.x <= xmax and
+                ymin <= point.y and point.y <= ymax):
+                found_points.append(point)
+
+        if self.is_divide:
+            self.TL.query(cx, cy, w, h, found_points)
+            self.TR.query(cx, cy, w, h, found_points)
+            self.BL.query(cx, cy, w, h, found_points)
+            self.BR.query(cx, cy, w, h, found_points)
+        return found_points
+    
+    def query_circle(self, cx, cy, radius, found_points):
+        xmin = cx - radius
+        xmax = cx + radius
+        ymin = cy - radius
+        ymax = cy + radius
+
+        if (xmax < self.xmin or xmin > self.xmax or
+            ymax < self.ymin or ymin > self.ymax):
+            return False
+
+        for point in self.points:
+            if (xmin <= point.x and point.x <= xmax and
+                ymin <= point.y and point.y <= ymax and
+                point.distance_to(Point(cx, cy)) <= radius):
+                found_points.append(point)
+
+        if self.is_divide:
+            self.TL.query_circle(cx, cy, radius, found_points)
+            self.TR.query_circle(cx, cy, radius, found_points)
+            self.BL.query_circle(cx, cy, radius, found_points)
+            self.BR.query_circle(cx, cy, radius, found_points)
+        return found_points
     
     def __str__(self) -> str:
         ret = "{}, {} :: ".format(self.depth, self.is_divide)
